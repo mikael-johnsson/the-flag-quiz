83fb65ca9a451da7618674ca9f076763b6bdab54
Mikael Johnsson <mikaeljohanjohnsson@gmail.com>
2024-03-18 08:11:39 +0000

    add function structure to js

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByClass("difficulty-buttons")
+})
+
+function runGame() {}
+
+function displayQuestion(){}
+
+function compareAnswer(){}
+
+function countCorrectAnswer(){}
+
+function endMessage(){}
