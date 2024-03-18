e2d0619b63b2ebe9cfbbff3b176fbd068dee4c76
Mikael Johnsson <mikaeljohanjohnsson@gmail.com>
2024-03-18 08:28:26 +0000

    add easy flags to array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,16 @@
+// Flag arrays
+
+let easyFlags = [
+    {name: "Argentina", flag: "assets/images/flags-easy/argentina.png"},
+    {name: "Austria", flag: "assets/images/flags-easy/austria.png"},
+    {name: "Brazil", flag: "assets/images/flags-easy/brazil.png"},
+    {name: "Canada", flag: "assets/images/flags-easy/canada.png"},
+    {name: "China", flag: "assets/images/flags-easy/china.png"},
+]
+
 document.addEventListener("DOMContentLoaded", function(){
     let buttons = document.getElementsByClass("difficulty-buttons")
+
 })
 
 function runGame() {}
