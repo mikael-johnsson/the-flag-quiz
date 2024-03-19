3ad9fd179c6c163744bb6d839d1df7d1302139a0
Mikael Johnsson <mikaeljohanjohnsson@gmail.com>
2024-03-19 09:24:56 +0000

    add answers to easyFlags array + modify displayQuestion function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,17 @@
 
 // Flag arrays
 let easyFlags = [
-    {name: "Argentina", flag: "assets/images/flags-easy/argentina.png"},
-    {name: "Austria", flag: "assets/images/flags-easy/austria.png"},
-    {name: "Brazil", flag: "assets/images/flags-easy/brazil.png"},
-    {name: "Canada", flag: "assets/images/flags-easy/canada.png"},
-    {name: "China", flag: "assets/images/flags-easy/china.png"},
-    {name: "Czech Republic", flag: "assets/images/flags-easy/czech_republic.png"},
-    {name: "France", flag: "assets/images/flags-easy/france.png"},
-    {name: "Italy", flag: "assets/images/flags-easy/italy.png"},
-    {name: "Japan", flag: "assets/images/flags-easy/japan.png"},
-    {name: "Mexico", flag: "assets/images/flags-easy/mexico.png"},
-]
+    {name: "Argentina", flag: "assets/images/flags-easy/argentina.png", answers:["Argentina", "Mexico", "China", "Japan", "Canada"]},
+    {name: "Austria", flag: "assets/images/flags-easy/austria.png", answers:["Austria", "China", "Italy", "France", "Canada"]},
+    {name: "Brazil", flag: "assets/images/flags-easy/brazil.png", answers:["Brazil", "Mexico", "Czech Republic", "Japan", "Canada"]},
+    {name: "Canada", flag: "assets/images/flags-easy/canada.png", answers:["Canada", "Argentina", "Austria", "Czech Republic", "Brazil"]},
+    {name: "China", flag: "assets/images/flags-easy/china.png", answers:["China", "France", "Italy", "Japan", "Canada"]},
+    {name: "Czech Republic", flag: "assets/images/flags-easy/czech_republic.png", answers:["Czech Republic", "Canada", "Argentina", "Austria", "Italy"]},
+    {name: "France", flag: "assets/images/flags-easy/france.png", answers: ["France", "Brazil", "Mexico", "Argentina", "Austria"]},
+    {name: "Italy", flag: "assets/images/flags-easy/italy.png", answers: ["Italy", "Mexico", "Japan", "China", "Brazil"]},
+    {name: "Japan", flag: "assets/images/flags-easy/japan.png", answers: ["Japan", "Czech Republic", "Italy", "France", "Brazil"]},
+    {name: "Mexico", flag: "assets/images/flags-easy/mexico.png", answers: ["Mexico", "France", "Czech Republic", "China", "Austria"]}
+];
 
 // Wait for DOM to load, then get difficulty input
 document.addEventListener("DOMContentLoaded",function(){
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded",function(){
     for (let button of buttons){
         button.addEventListener("click",function(){
             if (this.getAttribute("id") === "easy-button"){
-                flags = easyFlags
+                flags = easyFlags;
                 alert("You clicked Easy");
             } else if (this.getAttribute("id") === "moderate-button"){
                 //flags = moderateFlags
@@ -38,28 +38,27 @@ document.addEventListener("DOMContentLoaded",function(){
 
 function runGame(flags) {
     //To display the game-div
-    document.getElementById("start-div").style.display = "none"
-    document.getElementById("game-div").style.display = "block"
+    console.log(flags);
+    document.getElementById("start-div").style.display = "none";
+    document.getElementById("game-div").style.display = "block";
 
-    //To generate flag
-    let flag = Math.floor(Math.random() * flags.length + 1);
-    let question = flags[flag];
-    document.getElementById("flag").src = question.flag; //correct answer is now in question.name
-
-    //to generate options
-    let optionButtons = document.getElementsByClassName("option");
-    let i = 0;
-    while (i <= 4){
-        let country = Math.floor(Math.random() * flags.length + 1);
-        optionButtons[i].textContent = flags[country].name 
-        i++;
-    }
-    
-    
-     
+   
 }
 
 function displayQuestion(){
+     //To generate flag
+     let flag = Math.floor(Math.random() * flags.length + 1);
+     let question = flags[flag];
+     document.getElementById("flag").src = question.flag; //correct answer is now in question.name and one of the answers
+ 
+     //to generate options
+     let optionButtons = document.getElementsByClassName("option");
+     let i = 0;
+     while (i <= 4){
+         let option = Math.floor(Math.random() * flags[answers].length + 1);
+         optionButtons[i].textContent = flags[option].name 
+         i++;
+     }
 
 }
 
